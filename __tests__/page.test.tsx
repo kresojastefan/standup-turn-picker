c7651c8d120e286640b4ba5c5303792fa7247fbc
Stefan Kresoja <stefan.kresoja@protals.com>
2024-08-12 11:50:52 +0200

    removing joke test

--- __tests__/page.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { initialUsers } from '../app/constants';
-
-describe('MOST IMPORTAT TESTS', () => {
-  it('Will Florian be picked?', () => {
-    expect(initialUsers).toContain("Florian");
-  });
-});
